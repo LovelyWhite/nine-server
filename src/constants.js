cbd498cc30f546e741d6ccbe2d3ac100d20eea20
Wink Zhang <icezxn@qq.com>
2023-04-24 10:43:46 +0800

    base: init

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export default {
+    APPID: '***REMOVED***',
+    SECRET: '***REMOVED***',
+    TEMPLATEID: '_tdnD2Jxts6LxlmF9Z5QFY3RCJiHSGm1VdWxTAHWM_U',
+    USERLIST: {
+        nine: 'oibi95_MZoGDv2BWae0G3LMfOHj8',
+        suxi: 'oibi95wzssliUZdqjWRc7G3iVuOs',
+    }
+}
